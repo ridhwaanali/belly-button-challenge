dd9988483c611e75a1205d32ea32821632f86daf
Ridhwaan Ali <129802262+ridhwaanali@users.noreply.github.com>
2023-10-13 17:30:41 -0400

    Update edited_app.js

--- edited_app.js
+++ edited_app.js
@@ -1,5 +1,5 @@
 function getPlot(id){
-    d3.json("samples.json").then (sampledata=>{
+    d3.json("../samples.json").then (sampledata=>{
         console.log(sampledata)
         var ids= sampledata.samples[0].otu_ids;
         console.log(ids)
